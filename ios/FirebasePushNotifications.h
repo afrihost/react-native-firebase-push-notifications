9f58834ba501e4ae6ee6e5cfde4f1f3ad9bd05f2
Sacheen Dhanjie <sacheen@afrihost.com>
2020-01-30 13:47:05 +0200

    iOS implementation for push notifications

--- ios/FirebasePushNotifications.h
+++ ios/FirebasePushNotifications.h
@@ -1,5 +1,26 @@
-#import <React/RCTBridgeModule.h>
+#ifndef FirebasePushNotifications_h
+#define FirebasePushNotifications_h
+#import <Foundation/Foundation.h>
 
-@interface FirebasePushNotifications : NSObject <RCTBridgeModule>
+#if __has_include(<FirebaseMessaging/FirebaseMessaging.h>)
+#import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
+
+@interface FirebasePushNotifications : RCTEventEmitter<RCTBridgeModule>
+
++ (void)configure;
++ (_Nonnull instancetype)instance;
+
+#if !TARGET_OS_TV
+- (void)didReceiveLocalNotification:(nonnull UILocalNotification *)notification;
+- (void)didReceiveRemoteNotification:(nonnull NSDictionary *)userInfo fetchCompletionHandler:(void (^_Nonnull)(UIBackgroundFetchResult))completionHandler;
+#endif
 
 @end
+
+#else
+@interface FirebasePushNotifications : NSObject
+@end
+#endif
+
+#endif
