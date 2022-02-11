65eccaf2185305ccbd28d5c4bca6341e7e02d8ee
Franco Schwim <francoschwim@hotmail.com>
2022-02-11 14:50:32 +0200

    Added commented listeners

--- android/src/main/java/com/afrihost/firebase/notifications/RNFirebaseNotifications.java
+++ android/src/main/java/com/afrihost/firebase/notifications/RNFirebaseNotifications.java
@@ -66,6 +66,16 @@ public class RNFirebaseNotifications extends ReactContextBaseJavaModule implemen
     );
   }
 
+    // @ReactMethod
+  // public void addListener(String eventName) {
+  //   // For RN built in Event Emitter Calls.
+  // }
+
+  // @ReactMethod
+  // public void removeListeners(Integer count) {
+  //   // For RN built in Event Emitter Calls.
+  // }
+
   @Override
   public String getName() {
     return "RNFirebaseNotifications";
