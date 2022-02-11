6793f4a2793f953f71a7f1cf29dff8600e2aa3f6
Franco Schwim <francoschwim@hotmail.com>
2022-02-11 14:50:32 +0200

    Added commented listeners

--- android/src/main/java/com/afrihost/firebase/notifications/FirebasePushNotificationsModule.java
+++ android/src/main/java/com/afrihost/firebase/notifications/FirebasePushNotificationsModule.java
@@ -210,6 +210,16 @@ public class FirebasePushNotificationsModule extends ReactContextBaseJavaModule 
                 });
     }
 
+    // @ReactMethod
+  // public void addListener(String eventName) {
+  //   // For RN built in Event Emitter Calls.
+  // }
+
+  // @ReactMethod
+  // public void removeListeners(Integer count) {
+  //   // For RN built in Event Emitter Calls.
+  // }
+
     @ReactMethod
     public void cancelAllNotifications(Promise promise) {
         notificationManager.cancelAllNotifications(promise);
